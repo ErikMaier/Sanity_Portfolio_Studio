1829999aa08c4bdfe0f98ca100fa5c80d67f7f1c
Erik Maier <erikmaier80@gmail.com>
2022-02-02 13:19:49 +0100

    adjusted prettier

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
+import image from '../ErikMaierCVDesignDarker.jpg';
 
 export default function Home() {
-  return <h1>Home Page!</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Background Image"
+        className="absolute object-cover w-full h-full"
+      />
+      <section>
+        <h1>Hello. I'm Erik.</h1>
+      </section>
+    </main>
+  );
 }
